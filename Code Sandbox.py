3779e99fd763e41a0281e108a78d116292837671
lookitdown <othman.infotech@gmail.com>
2023-06-08 21:05:35 -0700

    Nicer Gui Buttons

--- Code Sandbox.py
+++ Code Sandbox.py
@@ -1,6 +1,3 @@
-import tkinter as tk
-from tkinter import ttk
-from ttkthemes import ThemedStyle
 import customtkinter
 
 def city_selection(state):
@@ -13,37 +10,27 @@ def city_selection(state):
     }
     
     # Hide the state buttons
-    for state_button in state_buttons:
-        state_button.destroy()
+    for state_button in state:
+        state_button.pack_forget()
     
     # Create buttons for each city in the selected state
     for city in cities[state]:
         city_button = ttk.Button(selection_frame, text=city, command=lambda c=city: print("Selected city:", c))
         city_button.pack(pady=5)
 
-# Create the main window
-root = tk.Tk()
-root.title("State and City Selection")
+app = customtkinter.CTk()
+app.geometry("800x300")
 
-# Create a frame to hold the state and city selection buttons
-selection_frame = ttk.Frame(root)
-selection_frame.pack()
+button1 = customtkinter.CTkButton(app, text="California", command=city_selection)
+button1.pack(padx=20, pady=20)
 
-# Create a label for state selection
-state_label = ttk.Label(selection_frame, text="Select your state:", font=("Helvetica", 14, "bold"))
-state_label.pack(pady=10)
+button2 = customtkinter.CTkButton(app, text="Chicago", command=city_selection)
+button2.pack(padx=20, pady=20)
 
-# Create buttons for each state in America
-states = ["California", "New York", "Texas"]  # Add more states as needed
-state_buttons = []
-for state in states:
-    state_button = customtkinter.CTkButton(selection_frame, text=state, command=lambda s=state: city_selection(s))
-    state_button.pack(pady=5)
-    state_buttons.append(state_button)
+button3 = customtkinter.CTkButton(app, text="Texas", command=city_selection)
+button3.pack(padx=20, pady=20)
 
-# Set the theme using ThemedStyle
-style = ThemedStyle(root)
-style.set_theme("vista")  # Apply the 'vista' theme
+button3 = customtkinter.CTkButton(app, text="Michigan", command=city_selection)
+button3.pack(padx=20, pady=20)
 
-# Start the GUI event loop
-root.mainloop()
+app.mainloop()
--- Sandbox 2.py
+++ Sandbox 2.py
@@ -4,15 +4,18 @@ def button_callback():
     print("button clicked")
 
 app = customtkinter.CTk()
-app.geometry("400x150")
+app.geometry("800x300")
 
-button1 = customtkinter.CTkButton(app, text="my button", command=button_callback)
+button1 = customtkinter.CTkButton(app, text="California", command=button_callback)
 button1.pack(padx=20, pady=20)
 
-button2 = customtkinter.CTkButton(app, text="my button", command=button_callback)
+button2 = customtkinter.CTkButton(app, text="Chicago", command=button_callback)
 button2.pack(padx=20, pady=20)
 
-button3 = customtkinter.CTkButton(app, text="my button", command=button_callback)
+button3 = customtkinter.CTkButton(app, text="Texas", command=button_callback)
+button3.pack(padx=20, pady=20)
+
+button3 = customtkinter.CTkButton(app, text="Michigan", command=button_callback)
 button3.pack(padx=20, pady=20)
 
 app.mainloop()
